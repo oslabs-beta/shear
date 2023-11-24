7fb220d97e5be9d9fae425552d0c0a2c422fc2c6
Caleb-Kao <kcaleb101@gmail.com>
2023-11-23 18:42:09 -0800

    functionality complete

---
+++ src/components/graph.tsx
@@ -0,0 +1,128 @@
+import { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+
+const App = () => {
+
+    const [timeData, setTimeData] = React.useState<number[]>([500, 420, 350, 290, 240, 200, 170, 150, 135, 125, 120]);
+    const [costData, setCostData] = React.useState<number[]>([180, 190, 202, 203, 201, 202, 203, 190, 185, 190, 200])
+    const [xData, setXData] = React.useState<number[]>([128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280, 2048]);
+    const xLabel = function (i) { return xData[i] }
+    const svgRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        // setting up svg
+        let width = 800;
+        let height = 600
+        let heightLeft = Math.max(...timeData) + 50;
+        let heightRight = Math.max(...costData) + 150;
+
+        const svg = d3
+            .select(svgRef.current)
+            .attr("width", width)
+            .attr("height", height)
+            .style("overflow", "visible")
+            .style("background", "#c7cab6")
+
+        // x-axis range
+        const xScale = d3
+            .scaleLinear()
+            .domain([0, timeData.length - 1])
+            .range([0, width]);
+
+        //y-axes range
+        const yScaleLeft = d3
+            .scaleLinear()
+            .domain([0, heightLeft])
+            .range([height, 0]);
+
+        const yScaleRight = d3
+            .scaleLinear()
+            .domain([0, heightRight])
+            .range([height, 0]);
+
+        //  Setup functions to draw Lines ---------------//
+        const invocationTime = d3
+            .line()
+            .x((d, i) => xScale(i))
+            .y(yScaleLeft)
+            .curve(d3.curveCardinal);
+
+        const runtimeCost = d3
+            .line()
+            .x((d, i) => xScale(i))
+            .y(yScaleRight)
+            .curve(d3.curveCardinal);
+
+        // setting the axes
+        const xAxis = d3.axisBottom(xScale).tickFormat((i) => xLabel(i));
+        const yAxisLeft = d3.axisLeft(yScaleLeft).ticks(10);
+        // var yAxisRight = d3.svg.axis().scale(y1)
+        //   .orient("right").ticks(5).tickSize(-width);
+        const yAxisRight = d3.axisRight(yScaleRight).ticks(10);
+
+        // svg.append("g").call(xAxis).attr("transform", `translate(0,${heightLeft})`);
+        // svg.append("g").call(yAxisLeft);
+        svg.append("g").attr("transform", `translate(${width},0)`).call(yAxisRight);
+
+        // setting up the data for the svg
+        svg
+            .selectAll(".line")
+            .data([timeData])
+            .join("path")
+            .attr("d", (d) => invocationTime(d))
+            .attr("fill", "none")
+            .attr("stroke", "blue");
+
+        svg
+            .selectAll(".line")
+            .data([costData])
+            .join("path")
+            .attr("d", (d) => runtimeCost(d))
+            .attr("fill", "none")
+            .attr("stroke", "red");
+
+        // axis labels
+        const yAxisLeftLabel = svg.append("g").classed("yAxis", true).call(yAxisLeft);
+        const yAxisRightLabel = svg.append("g").attr("transform", `translate(${width},0)`).call(yAxisRight);
+        const xAxisLabel = svg.append("g").call(xAxis).attr("transform", `translate(0,${height})`);
+
+        yAxisLeftLabel
+            .append("text")
+            .attr("fill", "black")
+            .text('Invocation Time')
+            .attr("x", -(height / 2))
+            .attr("y", -50)
+            .style("transform", "rotate(270deg)")
+            .style("text-anchor", "middle")
+            .style("font-size", "1.2rem");
+
+        yAxisRightLabel
+            .append("text")
+            .attr("fill", "black")
+            .text('Runtime Cost')
+            .attr("x", -(height / 2))
+            .attr("y", 50)
+            .style("transform", "rotate(270deg)")
+            .style("text-anchor", "middle")
+            .style("font-size", "1.2rem");
+
+        xAxisLabel
+            .append("text")
+            .attr("fill", "black")
+            .text('Memory (MB)')
+            .attr("x", width / 2)
+            .attr("y", 50)
+            .style("text-anchor", "middle")
+            .style("font-size", "1.2rem");
+    }, [timeData, costData, xData]);
+
+
+    return (
+        <div className="chartWrapper">
+            <h2>Line Charts</h2>
+            <svg className="svgWrap" ref={svgRef} style={{ margin: "100px", display: "block" }}></svg>
+        </div>
+    );
+};
+
+export default App;
