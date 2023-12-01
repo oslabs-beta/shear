d91f913987944cdb24febb08387f09c478b6767d
Albert Hu <albhu24@gmail.com>
2023-12-01 10:06:43 -0800

    Updating ts config file, initial server and router structure finished, also updating package.json

---
+++ src/server/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import {router} from "./routes.js"
+import path from "path"
+import { fileURLToPath } from 'url';
+
+const PORT = 3000;
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+
+// Importing route
+app.use("/api", router);
+
+
+// Path to serve static assests
+app.use(express.static(path.join(__dirname, '../../src/dist')));
+
+
+app.listen(PORT, () => console.log('listening on PORT', PORT));
