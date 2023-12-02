8f48d77b9f8ace393ea1d03030bd39ec3aebb2ac
jonbingkim <jonkimtw@gmail.com>
2023-12-02 09:50:29 -0800

    added component to APP.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import store from './store.ts'
+import App from './App.js'
+import store from './store.js'
 import { Provider } from 'react-redux'
 
 
