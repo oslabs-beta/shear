9b933b45c55cee52b1339e66208bd35043cfbc01
jonbingkim <jonkimtw@gmail.com>
2023-12-07 18:49:13 -0800

    changing up logic to see if 403 error still persists

--- src/formData/infoAPI.ts
+++ src/formData/infoAPI.ts
@@ -4,10 +4,6 @@ import { FormValues } from "./infoSlice";
 //Use this to make the baseURL of the fetch call so you can just add the string end point. - JK
 axios.defaults.baseURL = "http://localhost:3000/api";
 
-// export const optimizerAPI = {
-//   runOptimizerFunc: (formData: FormValues): Promise<AxiosResponse> => axios.post('executeLambdaWorkflow', formData),
-// };
-
 export const optimizerAPI = {
   runOptimizerFunc: (): Promise<AxiosResponse> => axios.post('executeLambdaWorkflow', {
     "memoryArray" : [128, 256, 512, 680],
