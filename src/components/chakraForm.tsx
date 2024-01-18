1b2cb0e8360398a928e1c194e3c8b2f3b046c13c
Caleb-Kao <kcaleb101@gmail.com>
2024-01-17 18:59:07 -0800

    commit before merge

--- src/components/chakraForm.tsx
+++ src/components/chakraForm.tsx
@@ -26,6 +26,15 @@ const ChakraForm: React.FC = () => {
     const onChangeMaxVal = (e: ChangeEvent<HTMLInputElement>) => {
         memoryRef.current[1] = e.target.value
     }
+
+    const onChangeIncrements = (e: ChangeEvent<HTMLInputElement>) => {
+        memoryRef.current[2] = e.target.value
+    }
+
+    const onChangeTestVol = (e: ChangeEvent<HTMLInputElement>) => {
+        memoryRef.current[3] = e.target.value
+    }
+
     //onSubmit changes the form state then invokes post request to backend -JK
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -33,51 +42,51 @@ const ChakraForm: React.FC = () => {
         dispatch(arnInput(arnRef.current?.value || ''));
         dispatch(funcParamsInput(funcParamsRef.current?.value || ''));
         dispatch(powerValueInput(memoryRef.current));
-        
 
-       
+
+
     };
 
 
     return (
         <div>
-            {/* This is just design for now (no functionality) */}
             <ChakraUI.Box position="relative" h="20%" w="60%" border="2px" padding="50px" borderRadius='lg' overflow='hidden' bg='lightgrey' margin='0 auto'>
-
                 <ChakraUI.Stack spacing={4}>
-
                     <ChakraUI.Text as='b' fontSize='24px' color='#4285F4'>Amazon stuff</ChakraUI.Text>
-
                     <ChakraUI.InputGroup>
                         <ChakraUI.InputLeftAddon children='ARN' bg='gray.400' />
                         <ChakraUI.Input ref={arnRef} placeholder='arn:aws:iam::123456789012:user/johndoe' />
                     </ChakraUI.InputGroup>
-
-                    {/* <ChakraUI.InputGroup >
-                        <ChakraUI.InputLeftAddon children='Function Parameters Keys' bg='gray.400' />
-                        <ChakraUI.Input ref={funcParamsRef} placeholder='Separate entries by commas' />
-                    </ChakraUI.InputGroup> */}
                     <ChakraUI.Text as='b' fontSize='24px' color='#4285F4'>Function Parameters</ChakraUI.Text>
                     <ChakraUI.Textarea
                         ref={funcParamsRef}
                         placeholder='Please enter parameters and values in JSON format'
                         size='sm'
                     />
-
                     <ChakraUI.Text as='b' fontSize='24px' color='#4285F4'>Memory Allocation</ChakraUI.Text>
                     {/* <ChakraUI.FormLabel>Power Values</ChakraUI.FormLabel> */}
                     <ChakraUI.HStack>
-                    <ChakraUI.InputGroup>
-                        <ChakraUI.InputLeftAddon children='Min' bg='gray.400' />
-                        <ChakraUI.Input type="text" onChange={onChangeMinVal} placeholder='128' />
-                    </ChakraUI.InputGroup>
-                    <ChakraUI.InputGroup>
-                        <ChakraUI.InputLeftAddon children='Max' bg='gray.400' />
-                        <ChakraUI.Input type="text"onChange={onChangeMaxVal} placeholder='4096' />
-                    </ChakraUI.InputGroup>
+                        <ChakraUI.InputGroup>
+                            <ChakraUI.InputLeftAddon children='Min' bg='gray.400' />
+                            <ChakraUI.Input type="text" onChange={onChangeMinVal} placeholder='128' />
+                        </ChakraUI.InputGroup>
+                        <ChakraUI.InputGroup>
+                            <ChakraUI.InputLeftAddon children='Max' bg='gray.400' />
+                            <ChakraUI.Input type="text" onChange={onChangeMaxVal} placeholder='4096' />
+                        </ChakraUI.InputGroup>
+                    </ChakraUI.HStack>
+                    <ChakraUI.HStack>
+                        <ChakraUI.InputGroup>
+                            <ChakraUI.InputLeftAddon children='Memory Intervals' bg='gray.400' />
+                            <ChakraUI.Input type="text" onChange={onChangeIncrements} placeholder='8' />
+                        </ChakraUI.InputGroup>
+                        <ChakraUI.InputGroup>
+                            <ChakraUI.InputLeftAddon children='Test Volume' bg='gray.400' />
+                            <ChakraUI.Input type="text" onChange={onChangeTestVol} placeholder='10' />
+                        </ChakraUI.InputGroup>
                     </ChakraUI.HStack>
                 </ChakraUI.Stack>
-                
+
                 <ChakraUI.Button
                     mt={4}
                     colorScheme='blue'
--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -14,73 +14,36 @@ export default function Graph() {
     const costData = graphData.CostData;
     const xData = graphData.MemoryData;
 
+    //converts raw data into appropriate format for graph
     const convertData = () => {
         const temp = []
         for (let i = 0; i < xData.length; i++) {
             temp.push({
-                memory: xData[i],
-                invocation_time: timeData[i],
-                runtime_cost: costData[i]
+                'Memory': xData[i],
+                'Invocation time': timeData[i],
+                'Runtime cost': costData[i]
             })
         }
         setData(temp)
     }
 
-    const [data, setData] = useState([
-        {
-            memory: 128,
-            invocation_time: 4000,
-            runtime_cost: 2400,
-        },
-        {
-            memory: 256,
-            invocation_time: 3000,
-            runtime_cost: 1398,
-        },
-        {
-            memory: 512,
-            invocation_time: 2000,
-            runtime_cost: 9800,
-        },
-        {
-            memory: 768,
-            invocation_time: 2780,
-            runtime_cost: 3908,
-        },
-        {
-            memory: 1024,
-            invocation_time: 1890,
-            runtime_cost: 4800,
-        },
-        {
-            memory: 2048,
-            invocation_time: 2390,
-            runtime_cost: 3800,
-        },
-        {
-            memory: 4096,
-            invocation_time: 3490,
-            runtime_cost: 4300,
-        }
-    ]);
-
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         convertData()
     }, [graphData])
 
+    // Recharts code; largely self-explanatory
     return (
         <div>
             <ResponsiveContainer
                 width="100%"
                 height={600}
             >
-
                 <LineChart
-                    width={1000}
+                    width="100%"
                     height={600}
                     data={data}
-
                     margin={{
                         top: 5,
                         right: 30,
@@ -89,7 +52,7 @@ export default function Graph() {
                     }}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="memory" />
+                    <XAxis dataKey="Memory" />
                     <YAxis
                         yAxisId="left"
                         label={{
@@ -115,21 +78,18 @@ export default function Graph() {
                     <Legend />
                     <Line
                         yAxisId="left"
-
                         type="monotone"
-                        dataKey="invocation_time"
+                        dataKey='Invocation time'
                         stroke="#8884d8"
                         activeDot={{ r: 8 }}
                     />
                     <Line yAxisId="right"
-
                         type="monotone"
-                        dataKey="runtime_cost"
+                        dataKey="Runtime cost"
                         stroke="#82ca9d" />
                 </LineChart>
-
             </ResponsiveContainer>
-            <button onClick={convertData}>Testtest</button>
+            {/* <button onClick={convertData}>Testtest</button> */}
         </div>
     );
 };
