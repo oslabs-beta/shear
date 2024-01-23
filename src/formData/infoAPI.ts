199ea5c5ec2dac5fd77f19880f53642cfe8d64ab
jonbingkim <jonkimtw@gmail.com>
2024-01-23 15:55:31 -0800

    changing port values

--- src/formData/infoAPI.ts
+++ src/formData/infoAPI.ts
@@ -6,7 +6,7 @@ import { FormValues } from "./infoSlice.js";
 // Use this to make the baseURL of the fetch call so you can just add the string endpoint. - JK 
 // for deployment, use port 443 || 80 so that you dont have to type the port into the baseURL
 // EX axios.defaults.baseURL =  https://localhost:443/api || http://localhost:80/api  -- this way it listens to the HTTPS first and goes to http if HTTPS is not present.
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "http://localhost:80/api";
 
 export const optimizerAPI = {
   runOptimizerFunc: (form: FormValues): Promise<AxiosResponse> => {
